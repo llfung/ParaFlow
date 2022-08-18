4f0ae2f02ba04a0aee5b094eb7d6f0dc54af23e0
Lloyd Fung <lloyd.fung12@imperial.ac.uk>
2022-08-18 17:01:35 +0100

    Added H-based Solver

--- template_caller.m
+++ template_caller.m
@@ -9,38 +9,47 @@ addpath(genpath('./app/'));
 %% ------ Initialization --------------------------------------------------
 % Parameters
 settings.Re=0.126;       % Reynolds number based on half width and swimming speed
-settings.Gamma=180;      % Dimensionless gravitational force (Richardson number)
-settings.Pe_s=0.5;
-settings.Q=-4;          % Flow rate setting
-
+settings.Gamma=0.1;      % Dimensionless gravitational force (Richardson number)
+settings.Pe_s=1;
+settings.Q=-0.5;          % Flow rate setting
 %% ------ Mesh Initialization ---------------------------------------------
-N_mesh=175; %Number of mesh points (in r direction)
+N_mesh=101; %Number of mesh points (in r direction)
 
 % Generating Chebyshev Collocation Points and Differential Matrices
-mesh=chebyshev(N_mesh,4,bc_type.none,tran_type.none);
+mesh=chebyshev(N_mesh,2,bc_type.none,tran_type.lin);
 % mesh=cfd6(N_mesh,4,bc_type.none);
 
+%% ------ Profile Initialization ------------------------------------------
+G0=0;
+U0=(1-mesh.pts.^2)*3/4*settings.Q;
+%U0=cos(3*pi*mesh.pts/2)*3*pi/2*settings.Q;
+N0=ones(N_mesh,1);
+
+x0=[G0;U0;N0];
 %% ------ Solve for basic state--------------------------------------------
 % Solving for base flow
 settings.disp_iter=true;
 
-prof_obj=NSTrans_VDlib(mesh,settings);
+prof_obj=NSTransH_VDlib_sym(mesh,settings,x0);
 prof_obj=prof_obj.solve();
 
 U0=prof_obj.prof(2:N_mesh+1);
 N0=prof_obj.prof(N_mesh+2:2*N_mesh+1);
 
 %% Plotting
-figure(3);
+figure(1);
 subplot(2,1,1);
 % hold on;
 plot(mesh.col_pt,U0);
+ylabel('U(x)');
 % plot(cheb.col_pt,U0_G0);
 % hold off;
 
 subplot(2,1,2);
 % hold on;
 plot(mesh.col_pt,N0);
+xlabel('x');
+ylabel('N(x)');
 % plot(cheb.col_pt,H0_G0);
 % hold off;
 
--- template_continuation_caller.m
+++ template_continuation_caller.m
@@ -8,13 +8,13 @@ addpath('./core/profile/');
 addpath(genpath('./app/'));
 %% ------ Initialization --------------------------------------------------
 % Parameters
-settings.Re=0.126;      % Reynolds number based on half width and swimming speed
-settings.Gamma=345;      % Dimensionless gravitational force (Richardson number)
-settings.Pe_s=0.5;      % Swimming Peclet Number
-settings.Q=-4;          % Flow rate setting
+settings.Re=1;      % Reynolds number based on half width and swimming speed
+settings.Gamma=-10;      % Dimensionless gravitational force (Richardson number)
+settings.Pe_s=1;      % Swimming Peclet Number
+settings.Q=-.01;          % Flow rate setting
 
 %% ------ Mesh Initialization ---------------------------------------------
-N_mesh=151; %Number of mesh points (in r direction)
+N_mesh=175; %Number of mesh points (in r direction)
 
 % Generating Chebyshev Collocation Points and Differential Matrices
 mesh=chebyshev(N_mesh,4,bc_type.none,tran_type.lin);
@@ -22,28 +22,29 @@ mesh=chebyshev(N_mesh,4,bc_type.none,tran_type.lin);
 
 %% ------ Continuation ----------------------------------------------------
 settings.disp_iter=true;
-settings.epsilon=1e-5;
+settings.epsilon=1e-8;
 % Limiter (limiting the value of N(0).
 settings.limiter_value=1000; % Stop iteration if N(0)>1000
 settings.limiter_index=2*N_mesh+1; % Index for N(0)
 
-cont_obj=NSTrans_VDlib_contRi_sym(mesh,settings,0.2,500,5);
+cont_obj=NSTransH_VDlib_contRi_sym(mesh,settings,.1,1000,1);
 cont_obj=cont_obj.run_cont_forward();
 
 %% Plotting
-figure(1);
-plot(cont_obj.para_out,cont_obj.x_out(2*N_mesh+1,:));
+figure(4);hold on;
+%plot(cont_obj.para_out,cont_obj.x_out(1+N_mesh+floor((N_mesh+1)/2),:));
+plot(cont_obj.para_out,cont_obj.x_out(1+N_mesh,:));
 
 figure(2);
 subplot(2,1,1);
 % hold on;
-plot(mesh.col_pt,cont_obj.x_out(2:N_mesh+1,1:end));
+plot(mesh.col_pt,cont_obj.x_out(2:N_mesh+1,1:10:end));
 % plot(cheb.col_pt,U0_G0);
 % hold off;
 
 subplot(2,1,2);
 % hold on;
-plot(mesh.col_pt,cont_obj.x_out(N_mesh+2:2*N_mesh+1,1:end));
+plot(mesh.col_pt,cont_obj.x_out(N_mesh+2:2*N_mesh+1,1:10:end));
 % plot(cheb.col_pt,H0_G0);
 % hold off;
 
